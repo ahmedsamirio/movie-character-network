231aa5a11f5cbe2d4009c8fe685386ea8010c9dc
ahmedsamirio <ahmedsamirio95@gmail.com>
2021-08-06 14:11:31 +0200

    removed https from bootstrap scripts

--- wrangling_scripts/scrape_data.py
+++ wrangling_scripts/scrape_data.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pickle
+import string
 import re
 import requests
 
@@ -26,9 +27,25 @@ def clean_script(text):
     return text.replace(r'\r', '')
 
 
+def get_clean_title(name):
+    """Function to return a clean movie title."""
+    # remove " Script.html" from the end of the title
+    name = name[:-12]
+
+    # correct movie with format "Movie, The" to "The Movie"
+    if ', The' in name:
+        name = 'The ' + name.replace(', The', '')
+
+    # escape single quotes
+    name = name.replace("'", r"")
+    return name
+
+
+
 def get_script(relative_link):
     search_title = relative_link.split('/')[-1]
-    print('fetching %s' % search_title)
+    clean_title = get_clean_title(search_title)
+    print('fetching %s' % clean_title)
     script_front_url = BASE_URL + quote(relative_link)
     front_page_response = requests.get(script_front_url)
     front_soup = BeautifulSoup(front_page_response.text, "html.parser")
@@ -36,7 +53,7 @@ def get_script(relative_link):
     try:
         script_link = front_soup.find_all('p', align="center")[0].a['href']
     except IndexError:
-        print('%s has no script :(' % search_title)
+        print('%s has no script :(' % clean_title)
         return None, None, None
 
     if script_link.endswith('.html'):
@@ -45,7 +62,7 @@ def get_script(relative_link):
         script_soup = BeautifulSoup(requests.get(script_url).text, "html.parser")
         script_text = script_soup.find_all('td', {'class': "scrtext"})[0].get_text()
         script_text = clean_script(script_text)
-        return search_title.rstrip(' Script.html'), save_title, script_text
+        return clean_title, save_title, script_text
     else:
         print('%s is a pdf :(' % search_title)
         return None, None, None
@@ -63,18 +80,20 @@ if __name__ == "__main__":
 
     for p in paragraphs:
         relative_link = p.a['href']
-        search_title, save_title, script = get_script(relative_link)
-        if not script:
-            continue
+        if relative_link == "/Movie Scripts/Avventura, L' (The Adventure) Script.html":
+            search_title, save_title, script = get_script(relative_link)
+            if not script:
+                continue
 
-        script_file = os.path.join(SCRIPTS_DIR, save_title.strip('.html') + '.txt')
+            # save script text
+            # clean save title from any punctuations that preven creating a filename
+            save_title = save_title[:-5].translate(str.maketrans('', '', string.punctuation)) + '.txt'
+            script_file = os.path.join(SCRIPTS_DIR, save_title)
+            with open(script_file, 'w', encoding='utf-8') as outfile:
+                outfile.write(script)
 
-        # save script text
-        with open(script_file, 'w', encoding='utf-8') as outfile:
-            outfile.write(script)
-
-        # save mapping of movie name with script text file
-        movies_files[search_title] = script_file
+            # save mapping of movie name with script text file
+            movies_files[search_title] = script_file
 
     # save mapping to a picke file for easy loading
     movies_files_pkl =  open(os.path.join(SCRIPTS_DIR, MOVIES), 'wb')
